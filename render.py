f5f1d635e6add2e1e2d66fde9553148cf020148b
heitor <heitor@redbaron.local>
2018-09-14 14:35:02 +0100

    Move pyjade templates to jinja2

--- render.py
+++ render.py
@@ -1,8 +1,8 @@
 import os
-import jinja2
 import json
 
 from data import decrypt
+from jinja2 import Template
 
 
 cvdata = decrypt('data.bin', os.getenv('CVKEY'))
@@ -10,9 +10,7 @@ cvdata = decrypt('data.bin', os.getenv('CVKEY'))
 for c in cvdata['publications']:
     c['authors'] = ', '.join(c['authors'])
 
-env = jinja2.Environment(extensions=['pyjade.ext.jinja.PyJadeExtension'])
-env.loader = jinja2.FileSystemLoader('.')
-template = env.get_template('template.html')
+template = Template(open('template.html').read())
 render = template.render(cv=cvdata)
 
 with open('cv.html', 'w') as frender:

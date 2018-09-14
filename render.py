81848eada7933f3db9018a48f2ffb57ab30cc8b6
heitor <heitor@redbaron.local>
2018-09-14 14:46:39 +0100

    Join publication authors on template

--- render.py
+++ render.py
@@ -4,14 +4,13 @@ import json
 from data import decrypt
 from jinja2 import Template
 
+def render():
+    cvdata = decrypt('data.bin', os.getenv('CVKEY'))
+    template = Template(open('template.html').read())
+    render = template.render(cv=cvdata)
+    with open('cv.html', 'w') as frender:
+        print(render, file=frender)
 
-cvdata = decrypt('data.bin', os.getenv('CVKEY'))
 
-for c in cvdata['publications']:
-    c['authors'] = ', '.join(c['authors'])
-
-template = Template(open('template.html').read())
-render = template.render(cv=cvdata)
-
-with open('cv.html', 'w') as frender:
-    print(render, file=frender)
+if __name__ == '__main__':
+    render()
